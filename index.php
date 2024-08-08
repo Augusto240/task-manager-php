8417e1b0093e135fd80b27acad75ec4c0d3bb18a
José Augusto de Oliveira Neto <20231148060001@ifrn.local>
2024-08-08 15:31:50 -0300

    changing some stuff on page layout

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciador de Tarefas</title>
+    <title>Gerenciador de Tarefaaaass :D</title>
 </head>
 <body>
-    <h1>Gerenciador de Tarefas</h1>
+    <h1>Gerenciador de Tarefas (Task Manager)</h1>
     <form action="add_task.php" method="post">
         <input type="text" name="task" placeholder="Nova tarefa" required>
         <button type="submit">Adicionar</button>
